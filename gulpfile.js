64bfd013fe5891b48b591eb0cd244b594b130641
frederikkelaumann <199391@rts-365.dk>
2020-05-25 13:25:24 +0200

    altid godt at committe

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const connect = require('gulp-connect');
+const { watchHTML } = require('./tasks/html');
+
+
+
+function dev (){
+    watchHTML();
+    connect.server({
+        livereload: true,
+        port: 3000,
+        root: "tmp"
+    })
+ 
+}
+exports.default = dev;
